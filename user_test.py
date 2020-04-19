05f3cb09f6a399c57aa74c5d1e42874cdee679da
AkinyiOwuor <owuorrachel@gmail.com>
2020-04-19 13:44:17 +0300

    add test_save_multiple_user test case

--- user_test.py
+++ user_test.py
@@ -25,10 +25,38 @@ class TestUser(unittest.TestCase):
 
     def test_save_user(self):
             '''
-            test_save_user test case to test if the user object is saved into the user list.
+            test_save_user test case to test if the user object is saved into the user_list.
             '''
             self.new_user.save_user() # saving the new user
             self.assertEqual(len(User.user_list),1)
+
+    def test_save_multiple_user(self):
+        '''
+        test_save_multiple_user to check if we can save multiple user objects to our user_list.
+        '''
+        self.new_user.save_user()
+        test_user = User("Test","Rachel Owuor","facebook","Owuor@254") # new user
+        test_user.save_user()
+        self.assertEqual(len(User.user_list),2)
+
+    def tearDown(self):
+        '''
+        tearDown method that does clean up after each test case has run.
+        '''
+        User.user_list = []
+
+    # def test_delete_user(self):
+    #     '''
+    #     test_delete_user to test if we can remove a user from our user_list
+    #     '''
+    #     self.new_user.save_user()
+    #     test_user = User("Test","Rachel Owuor","facebook","Owuor@254") # new contact
+    #     test_user.save_user()
+    #     self.new_user.delete_user()# Deleting a user object
+    #     self.assertEqual(len(User.user_list),1)
+
+
+
 
 
 if __name__ == '__main__':
