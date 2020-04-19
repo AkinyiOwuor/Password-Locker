c0facc757693222e881a16d7560813133df26dfd
AkinyiOwuor <owuorrachel@gmail.com>
2020-04-19 12:13:03 +0300

    add user details method

--- user.py
+++ user.py
@@ -1,5 +1,13 @@
+import pyperclip
 class User:
     '''
     Class that generates new instances of users.
     '''
     user_list=[]# Empty user list
+
+    def __init__(self,user_name,email,phone_number,password):
+
+       self.user_name=user_name
+       self.email=email
+       self.phone_number=phone_number
+       self.password=password
